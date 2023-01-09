c08a5d89ac5bc92e98d469b652304ac736e09bed
mediatechid <adi16.agung@gmail.com>
2023-01-09 10:47:51 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { fetchUser } from "./utils";
+import { Logo } from "./Logo";
+import "./styles.css";
+
+export default function App() {
+  const user = fetchUser();
+
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <Logo />
+
+        {/* Write your code : 
+          - if the user is not logged in : render a button that says "login"
+
+          - if the user is logged in :
+            render a button that says logout and
+              - if the user has a username : render a greeting message with the username "Welcome username !"
+              - if the user does not have a user name : render "Logged and Please complete your username!""
+
+        */}
+      </nav>
+      
+    </div>
+  );
+}
---
+++ src/Logo.js
@@ -0,0 +1,7 @@
+export const Logo = () => (
+  <img
+    src="https://www.kodingakademi.id/wp-content/uploads/2020/11/logo-kodak-scaled-1.jpg.webp"
+    alt="Koding Akademi"
+    width="200"
+  />
+);
